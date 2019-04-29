8dcb65e47b2c8a4194e05a0fc08a78a4692e83fb
Manish068 <mkmk6499@gmail.com>
2019-04-30 02:29:22 +0530

    dark to light theme

--- app/src/main/java/com/example/operator/settinglayout.java
+++ app/src/main/java/com/example/operator/settinglayout.java
@@ -1,13 +1,44 @@
 package com.example.operator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class settinglayout extends AppCompatActivity {
 
+    private Switch myswitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.LightTheme);
+        }
+        else setTheme(R.style.DarkTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settinglayout);
+        myswitch=(Switch)findViewById(R.id.myswitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp(){
+        Intent i=new Intent(getApplicationContext(),settinglayout.class);
+        startActivity(i);
+        finish();
     }
 }
